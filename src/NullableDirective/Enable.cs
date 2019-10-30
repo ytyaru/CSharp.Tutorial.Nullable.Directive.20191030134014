ad3ccfd6652f9d3c058097a9411d0b919a15ddf6
ytyaru <ytyaru@outlook.jp>
2019-10-31 08:36:13 +0900

    修正

--- src/NullableDirective/Enable.cs
+++ src/NullableDirective/Enable.cs
@@ -8,11 +8,11 @@ namespace NullableDirective
         {
 #nullable enable
             int i1 = null; // error CS0037: Null 非許容の値型であるため、Null を 'int' に変換できません
-            // warning CS0219: 変数 'i2' は割り当てられていますが、その値は使用されていません。
-            // warning CS8600: Null リテラルまたは Null の可能性がある値を Null 非許容型に変換しています。
-            int? i2 = null; 
-            string s1 = null; // warning CS0219
-            string? s2 = null; // warning CS0219
+            int? i2 = null;
+            string s1 = null; // warning CS8600: Null リテラルまたは Null の可能性がある値を Null 非許容型に変換しています。
+            string? s2 = null;
+            string s3 = null!;
+            Console.WriteLine($"{i1}, {i2}, {s1}, {s2}, {s3}");
         }
     }
 }
--- src/NullableDirective/Restore.cs
+++ src/NullableDirective/Restore.cs
@@ -11,6 +11,7 @@ namespace NullableDirective
             int? i2 = null;
             string s1 = null;
             string? s2 = null;
+            Console.WriteLine($"{i1}, {i2}, {s1}, {s2}");
         }
     }
 }
